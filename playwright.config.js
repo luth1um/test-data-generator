afd7001a4da25e31e31b10aa0ce162f8edf0d668
luth1um <52032867+luth1um@users.noreply.github.com>
2025-08-30 22:07:01 +0200

    deactivated retries of E2E tests for pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -20,8 +20,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* No retries (not even on CI) */
+  retries: 0,
   /* Set number of parallel tests. */
   workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
