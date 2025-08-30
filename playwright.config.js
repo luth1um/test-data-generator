79fe89377a55f8ae61f60a6b33f659581d749f1f
luth1um <52032867+luth1um@users.noreply.github.com>
2025-08-30 21:59:08 +0200

    fine-tuning of Playwright for pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -22,10 +22,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Set number of parallel tests. */
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "github" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
