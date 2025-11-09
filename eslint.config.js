f1ca31aa7f727e9cbb536e476e0e4ee8b5c34901
luth1um <52032867+luth1um@users.noreply.github.com>
2025-11-09 16:15:42 +0100

    switched from eslint to oxlint

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import js from "@eslint/js";
-import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-  { ignores: ["dist"] },
-]);
