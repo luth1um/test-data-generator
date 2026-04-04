f3096015117b0fa481b0131e0860d2b397c6c661
luth1um <52032867+luth1um@users.noreply.github.com>
2026-04-04 14:55:15 +0200

    added sharding to speed up E2E execution in CI

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "list",
+  reporter: process.env.CI ? "blob" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
